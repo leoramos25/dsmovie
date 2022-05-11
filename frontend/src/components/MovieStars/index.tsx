c0562ab3a3a3d35165a9f5a0ce4fb6a3b597be65
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-05-11 14:47:16 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,16 @@
+import { ReactComponent as StarFull } from "assets/images/star-full.svg";
+import { ReactComponent as StarEmpty } from "assets/images/star-empty.svg";
+import { ReactComponent as StarHalf } from "assets/images/star-half.svg";
+import "./styles.css";
+
+export function MovieStars() {
+    return (
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+    )
+}
